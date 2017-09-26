c0c00d305b81761c1a937571ed7360e1b6d8f984
agmkowalczyk <agm.kowalczyk@gmail.com>
2017-09-26 21:47:18 +0200

    poprawiona funkcja browserSync

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,16 +3,14 @@ module.exports = function(grunt) {
 		// Project configuration.
 		grunt.initConfig ({
 
-
-
-		sass: {
-      		options: {
-        		sourceMap: true
-      		},
-      		dist: {
-       			files: {
-          			'css/style.css': 'sass/style.sass'
-        		}
+		  sass: {
+        options: {
+      	 sourceMap: true
+        },
+      	dist: {
+       		files: {
+        		'css/style.css': 'sass/style.sass'
+        	}
      		}
     	},
 
@@ -29,9 +27,10 @@ module.exports = function(grunt) {
 
       browserSync: {
         bsFiles: {
-          src : 'css/*.css'
+          src : ['css/*.css']
         },
         options: {
+          watchTask: true,
           server: {
             baseDir: "./"
           }
@@ -40,26 +39,20 @@ module.exports = function(grunt) {
 
     	watch: {
     		scripts: {
-    			files: ['sass/*.sass'],
+    			files: 'sass/*.sass',
     			tasks: ['sass'],
     			options: {
     				spawn: false,
     			},
     		}
     	}
-
-
-
 		});
 		// Load the plugins tasks
 		grunt.loadNpmTasks('grunt-sass');
 		grunt.loadNpmTasks('grunt-contrib-imagemin');
-		grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-browser-sync');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
-
-
 		// Default task(s).
-		grunt.registerTask('default', ['sass', 'imagemin', 'browserSync', 'watch']);
-
+   grunt.registerTask('default', ['sass', 'imagemin', 'browserSync', 'watch']);
 };
